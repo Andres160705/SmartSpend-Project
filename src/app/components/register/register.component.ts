308eb3f03956ae1154956398779e388ae11f8972
Andres <andreshurdato16@gmail.com>
2025-09-22 10:53:10 -0500

    Carga de ambiente de firebase

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  constructor(private router: Router) {}
+
+  irLogin(){
+    this.router.navigate(['/login']);
+  }
+
+
+}
---
+++ src/app/environments/firebase.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDhbIYJhaAp0ND1MCky8aCRzBzTJGv2jDg",
+        authDomain: "smartspend-16a9f.firebaseapp.com",
+        projectId: "smartspend-16a9f",
+        storageBucket: "smartspend-16a9f.firebasestorage.app",
+        messagingSenderId: "919071882612",
+        appId: "1:919071882612:web:341a1d7e8ce3a3bae5c021"
+    },
+};
